88e5264865456a395551442317eaf728c3c2a181
Al Rifat <hasanalrifat156@gmail.com>
2024-02-29 22:21:53 +0600

    first commit

---
+++ test.js
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+
+    <style>
+        .container {
+        	background-color: rgb(2, 201, 118);
+        	color: red;
+        }
+        
+        h2 {
+        	text-align: right;
+        	padding: 10x;
+        }
+    </style>
+</head>
+
+<body>
+    <h2 style="color:green">
+        GeeksforGeeks
+    </h2><br />
+
+    <div class="container mx-auto">
+        This is mx-auto class
+    </div>
+</body>
+
+</html>
